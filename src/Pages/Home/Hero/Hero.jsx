72320732e92fc51cd437f7531c3691074c593801
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-06-08 07:01:41 -0700

    created about us section

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -22,25 +22,25 @@ const Hero = () => {
                             <FaCalendar className="text-green-800"></FaCalendar>
                             <div>
                                 <h1>Check In</h1>
-                                <h1>date</h1>
+                                <input type="date" placeholder="Type here" className="input input-ghost w-full max-w-xs" />
                             </div>
                         </div>
                         <div className="flex text-center items-center gap-3 p-6 px-8 bg-white text-black border-r-4">
                             <FaCalendar className="text-green-800"></FaCalendar>
                             <div>
                                 <h1>Check In</h1>
-                                <h1>date</h1>
+                                <input type="date" placeholder="Type here" className="input input-ghost w-full max-w-xs" />
                             </div>
                         </div>
                         <div className="flex text-center items-center gap-3 p-6 px-8 bg-white text-black ">
                             <FaUserAlt className="text-green-800"></FaUserAlt>
                             <div>
-                                <h1>Check In</h1>
-                                <h1>date</h1>
+                                <h1>Guest</h1>
+                                <h1 className="text-green-800"> 4 Guest </h1>
                             </div>
                         </div>
                         <div className="bg-green-800 text-center gap-3 p-6 px-8  text-white">
-                            <h1 className="pt-3">Book Now</h1>
+                            <h1 className="pt-7">Book Now</h1>
                         </div>
                     </div>
                     </div>
