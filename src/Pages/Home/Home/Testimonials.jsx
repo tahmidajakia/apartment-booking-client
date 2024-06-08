d1838b14eb373f19666e7514828b6eb34b9fe566
Mst Tahmida Begum Jakia <devtahmidajakia@gmail.com>
2024-06-08 07:37:39 -0700

    created our facilities section

--- src/Pages/Home/Home/Testimonials.jsx
+++ src/Pages/Home/Home/Testimonials.jsx
@@ -1,30 +1,56 @@
-
-
-
-
-import img from '../../../assets/images/apartment10.jpg'
-import test from '../../../assets/images/test1.jpg'
+import img from '../../../assets/images/2374.png_1200 - Copy.png'
 
 const Testimonials = () => {
-    return (
-        <div className="max-w-screen-2xl container mx-auto xl:px-16 mt-5">
-           <div className="">
-             <div className="h-[500px] rounded-xl bg-no-repeat bg-cover" style={{backgroundImage:` linear-gradient(#0E1C26B3,#557C934D), url(${img})`}}>
-               <div className='text-center space-y-5  pt-28 text-white'>
-                  <h1 className='text-3xl'>Testimonials</h1>
-                  <div className='flex justify-center text-center'>
-                  <p className='w-1/2 flex justify-center text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima tenetur exercitationem animi et beatae deleniti odio numquam dolorem officiis asperiores? Eaque rerum atque distinctio qui nostrum officia! Nemo, sint molestiae.</p>
-                  </div>
-                  <div className='flex justify-center items-center'>
-                    <img className='w-[70px] rounded-full' src={test} alt="" />
-                  </div>
-                  <h1>Name</h1>
-               </div>
-           </div>
-           </div>
-            
+  return (
+    <div className="max-w-screen-2xl container mx-auto xl:px-16 mt-5">
+      <div className="flex flex-col md:flex-row items-center justify-between gap-12">
+        <div className="md:w-1/2">
+        <img src={img} alt="" />
         </div>
-    );
+        <div className="md:w-1/2">
+          <div className='space-y-4 md:w-4/5'>
+          <h1>TESTIMONIALS</h1>
+          <h1 className='text-4xl font-bold'>What Our Customers Say About Us</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis neque aspernatur sint ipsum aliquid accusantium quaerat, iusto dolor voluptatum deserunt!</p>
+          </div>
+
+
+          <div className='flex gap-3 mt-5'>
+              <div className="avatar-group -space-x-6 rtl:space-x-reverse">
+                <div className="avatar">
+                  <div className="w-12">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                   </div>
+                   </div>
+                <div className="avatar">
+                    <div className="w-12">
+                      <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+               </div>
+                   <div className="avatar">
+                     <div className="w-12">
+                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                     </div>
+                   </div>
+                  <div className="avatar placeholder">
+                       <div className="w-12 bg-neutral text-neutral-content">
+                     <span>+1.5k</span>
+                      </div>
+                     </div>
+                  </div>
+                  <div>
+                     <h1 className='font-bold text-2xl'>Customer Feedback</h1>
+                     <h1><div className="rating">
+                        <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+                        <h1 className='ml-3'>4.9 (18.6k Reviews)</h1>
+  
+                        </div></h1>
+                  </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonials;
